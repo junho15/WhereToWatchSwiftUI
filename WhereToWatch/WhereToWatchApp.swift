2ffa382ed4a4a3eb4d29a1ed69b156868d2125b8
junho <junhoL1046@gmail.com>
2024-03-19 20:55:05 +0900

    feat: 프로젝트 추가

---
+++ WhereToWatch/WhereToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereToWatchApp.swift
+//  WhereToWatch
+//
+//  Created by junho lee on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WhereToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
