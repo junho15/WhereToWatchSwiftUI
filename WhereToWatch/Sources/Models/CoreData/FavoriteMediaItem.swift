62bd278198ec506feb873b57096bb1ab309b289a
junho <junhoL1046@gmail.com>
2024-04-16 22:13:11 +0900

    feat: FavoriteMediaItem 추가

---
+++ WhereToWatch/Sources/Models/CoreData/FavoriteMediaItem.swift
@@ -0,0 +1,24 @@
+import Foundation
+import MovieDatabaseAPI
+
+struct FavoriteMediaItem: Identifiable {
+    let id: MediaItem.ID
+    let mediaType: MediaType
+    let registrationDate: Date?
+
+    init?(mediaItemEntity: MediaItemEntity) {
+        guard let mediaType = MediaType(rawValue: mediaItemEntity.mediaTypeText) else {
+            return nil
+        }
+
+        self.id = Int(mediaItemEntity.id)
+        self.mediaType = mediaType
+        self.registrationDate = mediaItemEntity.registrationDate
+    }
+
+    init(mediaItem: MediaItem, registrationDate: Date = Date()) {
+        self.id = mediaItem.id
+        self.mediaType = mediaItem.mediaType
+        self.registrationDate = registrationDate
+    }
+}
