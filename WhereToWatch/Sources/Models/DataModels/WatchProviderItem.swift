ea82ecefde4fdeecdbcc7e01588b3aacadbca2d1
junho <junhoL1046@gmail.com>
2024-04-26 19:12:12 +0900

    feat: WatchProviderItem 추가

---
+++ WhereToWatch/Sources/Models/DataModels/WatchProviderItem.swift
@@ -0,0 +1,22 @@
+import MovieDatabaseAPI
+
+struct WatchProviderItem: Identifiable {
+    let id: Int
+    let logoPath: String?
+    let providerName: String?
+    let displayPriority: Int?
+
+    init(id: Int, logoPath: String?, providerName: String?, displayPriority: Int?) {
+        self.id = id
+        self.logoPath = logoPath
+        self.providerName = providerName
+        self.displayPriority = displayPriority
+    }
+
+    init(watchProvider: WatchProvider) {
+        self.id = watchProvider.id
+        self.logoPath = watchProvider.logoPath
+        self.providerName = watchProvider.providerName
+        self.displayPriority = watchProvider.displayPriority
+    }
+}
