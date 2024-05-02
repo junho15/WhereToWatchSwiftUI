851e952a4af4e48cfa84a89ab4e0de311d269c09
junho <junhoL1046@gmail.com>
2024-05-02 19:38:50 +0900

    feat: MediaItemsListView 추가

---
+++ WhereToWatch/Sources/Views/MediaItemsListView.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct MediaItemsListView: View {
+    @Binding var mediaItems: [MediaItem]
+    @Binding var selectedMediaItem: MediaItem?
+    let enableDelete: Bool
+
+    var body: some View {
+        List {
+            ForEach(mediaItems) { mediaItem in
+                MediaItemView(mediaItem: mediaItem)
+                    .frame(maxHeight: Constants.maxHeight)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        selectedMediaItem = mediaItem
+                    }
+            }
+            .onDelete(perform: enableDelete ? deleteItem : nil)
+            .listRowSeparator(.hidden)
+            .listRowInsets(Constants.listRowInsets)
+        }
+    }
+
+    func deleteItem(at offset: IndexSet) {
+        mediaItems.remove(atOffsets: offset)
+    }
+}
+
+extension MediaItemsListView {
+    private enum Constants {
+        static let maxHeight = CGFloat(150)
+        static let listRowInsets = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+    }
+}
+
+#Preview {
+    @State var mediaItems = PreviewData.mediaItems
+    @State var selectedMediaItem: MediaItem?
+
+    return MediaItemsListView(
+        mediaItems: $mediaItems,
+        selectedMediaItem: $selectedMediaItem,
+        enableDelete: true
+    )
+}
