680a8ebb5bbe846f8f5a43223c7adc858cb3e6c3
junho <junhoL1046@gmail.com>
2024-05-24 22:42:09 +0900

    feat: Sortable 채택

--- WhereToWatch/Sources/Models/DataModels/MediaItem.swift
+++ WhereToWatch/Sources/Models/DataModels/MediaItem.swift
@@ -1,19 +1,20 @@
 import Foundation
 import MovieDatabaseAPI
 
-struct MediaItem: Identifiable, Equatable, Hashable {
+struct MediaItem: Identifiable, Equatable, Hashable, Sortable {
     let id: Int
     let mediaType: MediaType
     let posterPath: String?
     let backdropPath: String?
     let title: String?
     let genre: String?
-    let year: String?
+    let date: Date?
     let overview: String?
+    var favoriteRegistrationDate: Date?
 
     init(
         id: Int, mediaType: MediaType, posterPath: String?, backdropPath: String?,
-        title: String?, genre: String?, year: String?, overview: String?
+        title: String?, genre: String?, date: Date?, overview: String?, favoriteRegistrationDate: Date? = nil
     ) {
         self.id = id
         self.mediaType = mediaType
@@ -21,18 +22,20 @@ struct MediaItem: Identifiable, Equatable, Hashable {
         self.backdropPath = backdropPath
         self.title = title
         self.genre = genre
-        self.year = year
+        self.date = date
         self.overview = overview
+        self.favoriteRegistrationDate = favoriteRegistrationDate
     }
 
-    init(media: any MediaProtocol, genreList: GenreList?) {
+    init(media: any MediaProtocol, genreList: GenreList?, favoriteRegistrationDate: Date? = nil) {
         self.id = media.id
         self.mediaType = media.mediaType
         self.posterPath = media.posterPath
         self.backdropPath = media.backdropPath
         self.title = media.title
-        self.year = media.date?.year
+        self.date = media.date
         self.overview = media.overview
+        self.favoriteRegistrationDate = favoriteRegistrationDate
 
         guard let genreIds = media.genreIds else {
             self.genre = nil
@@ -47,10 +50,14 @@ struct MediaItem: Identifiable, Equatable, Hashable {
 
 extension MediaItem: ImageURLRepresentable {
     var posterURL: URL? {
-        return imageURL(path: posterPath, size: .w500)
+        imageURL(path: posterPath, size: .w500)
     }
 
     var backdropURL: URL? {
         imageURL(path: backdropPath, size: .w500)
+    }
+
+    var year: String? {
+        date?.year
     }
 }
